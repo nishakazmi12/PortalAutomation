ed2c180755eee71eea80698c2e276189b21ceaa2
Nisha Kazmi <59197825+nishakazmi12@users.noreply.github.com>
2023-10-18 13:26:05 +0500

    Update sourcefiles.py

--- Files/sourcefiles.py
+++ Files/sourcefiles.py
@@ -1,8 +1,8 @@
 import csv
 
 class source_file(object):
-    csv_filepath_valid_credentials = "C:\\Users\\nisha.kazmi\\OneDrive - Astera Software\\SQA\\REST-dotNet\\Publish\\ClientApp\\SeleniumCode\\APIAutomation\\SourceFiles\\LoginCredentials_Valid.csv"
-    csv_filepath_invalid_credentials = "C:\\Users\\nisha.kazmi\\OneDrive - Astera Software\\SQA\\REST-dotNet\\Publish\\ClientApp\\SeleniumCode\\APIAutomation\\SourceFiles\\LoginCredentials_Invalid.csv"
-    csv_filepath_valid_signUp_credentials = "C:\\Users\\nisha.kazmi\\OneDrive - Astera Software\\SQA\\REST-dotNet\\Publish\\ClientApp\\SeleniumCode\\APIAutomation\\SourceFiles\\SignUpCredentials_Valid.csv"
-    csv_filepath_invalid_signUp_credentials = "C:\\Users\\nisha.kazmi\\OneDrive - Astera Software\\SQA\\REST-dotNet\\Publish\\ClientApp\\SeleniumCode\\APIAutomation\\SourceFiles\\SignUpCredentials_Invalid.csv"
+    csv_filepath_valid_credentials = "SeleniumCode\\APIAutomation\\SourceFiles\\LoginCredentials_Valid.csv"
+    csv_filepath_invalid_credentials = "SeleniumCode\\APIAutomation\\SourceFiles\\LoginCredentials_Invalid.csv"
+    csv_filepath_valid_signUp_credentials = "SeleniumCode\\APIAutomation\\SourceFiles\\SignUpCredentials_Valid.csv"
+    csv_filepath_invalid_signUp_credentials = "SeleniumCode\\APIAutomation\\SourceFiles\\SignUpCredentials_Invalid.csv"
 
